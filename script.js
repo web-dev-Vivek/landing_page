9e112ea2ef15b6c56f5860c71f642bda3382f1fa
Vivek Jha <progamervivek2020@gmail.com>
2025-07-07 11:19:33 +0530

    .

---
+++ script.js
@@ -0,0 +1,26 @@
+let tl = gsap.timeline();
+tl.from("#card1 ", {
+  duration: 1,
+  x: 50,
+  opacity: 0,
+  ease: "power2.out",
+});
+
+tl.from("#card2 ", {
+  duration: 1,
+  x: 50,
+  opacity: 0,
+  ease: "power2.out",
+});
+tl.from("#card3 ", {
+  duration: 1,
+  x: 50,
+  opacity: 0,
+  ease: "power2.out",
+});
+tl.from("#card4 ", {
+  duration: 1,
+  x: 50,
+  opacity: 0,
+  ease: "power2.out",
+});
