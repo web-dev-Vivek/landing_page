7cf7a3099b531d73b23a8cfd7ac11d827d815247
Vivek Jha <progamervivek2020@gmail.com>
2025-07-08 00:09:28 +0530

    motion_done

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
+// Timeline intro animation
 let tl = gsap.timeline();
-tl.from("#card1 ", {
+
+tl.from("#card1", {
   duration: 1,
   x: 50,
   opacity: 0,
   ease: "power2.out",
 });
 
-tl.from("#card2 ", {
+tl.from("#card2", {
   duration: 1,
   x: 50,
   opacity: 0,
   ease: "power2.out",
 });
-tl.from("#card3 ", {
+
+tl.from("#card3", {
   duration: 1,
   x: 50,
   opacity: 0,
   ease: "power2.out",
 });
-tl.from("#card4 ", {
+
+tl.from("#card4", {
   duration: 1,
   x: 50,
   opacity: 0,
   ease: "power2.out",
 });
+
+function GSAP(el) {
+  document.querySelectorAll(".card").forEach((card) => {
+    if (card.classList.contains("z-[99]")) {
+      card.classList.remove("z-[99]");
+    }
+  });
+
+  var tl = gsap.timeline();
+  tl.from(el, {
+    rotation: 110,
+    opacity: 1,
+    duration: 1,
+    ease: "linear",
+    transformOrigin: "top center",
+  });
+  tl.call(
+    () => {
+      el.classList.add("z-[99]");
+    },
+    null,
+    "+=0.1"
+  );
+}
